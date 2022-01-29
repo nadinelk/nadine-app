ca3b4ce900d96bb9b8a2d27660014c9e6b7f84e1
nadinelk <nadinekurnadi@gmail.com>
2022-01-29 09:01:10 +0700

    added segment3

---
+++ src/components/Segment3.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Segment2 from "./components/Segment2"
+
+interface ISegment3 {
+  showPic = () => {
+    while (counter % 2 != 0) {
+
+    }
+  }
+}
+
+class Segment1 extends React.Component<ISegment3> {
+  render() {
+    return (
+      <img></img>
+    );
+  }
+}
+
+export default Segment1;
